973c2f466a9a0909e9f2f861fc8077a4ac3b37b9
OlamideLanre <lanreprincess@gmail.com>
2024-09-18 22:41:45 +0100

    restructured html & css clean up

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -4,19 +4,20 @@ const Weather = () => {
   const [city, setCity] = useState("");
   const [error, setError] = useState(null);
   const [bgImage, setBgImage] = useState("mainBG.jpg");
-  const API_KEY = "f354b189aaf77a355d65e2f002046f0b";
+  // const [weatherData, setWeatherData] = useState();
+  const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
   const REQUEST_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
   // GET DATE FUNCTION
-  //const [currentDate, setCurrentDate] = useState(getDate());
-  // function getDate() {
-  //   const todaysDate = new Date();
-  //   const month = todaysDate.getMonth() + 1;
-  //   const dateOfWeek = todaysDate.getDate();
-  //   const year = todaysDate.getFullYear();
+  const [currentDate, setCurrentDate] = useState(getDate());
+  function getDate() {
+    const todaysDate = new Date();
+    const month = todaysDate.getMonth() + 1;
+    const dateOfWeek = todaysDate.getDate();
+    const year = todaysDate.getFullYear();
 
-  //   return `${dateOfWeek}/${month}/${year}`;
-  // }
+    return `${dateOfWeek}/${month}/${year}`;
+  }
 
   // SETTING BACKGROUND IMAGES
   let clearSkyimg = "mainBG.jpg";
@@ -37,9 +38,12 @@ const Weather = () => {
       let response = await fetch(REQUEST_URL);
       let data = await response.json();
 
-      console.log(data);
-
       if (response.ok) {
+        // Storing data in a state
+        // const weather = data;
+        // setWeatherData(weather);
+        // console.log("humidity : " + weatherData.main.humidity);
+
         let Temperature = document.getElementsByClassName("temprature");
         let Feelslike = document.getElementsByClassName("feelslike");
         let wind = document.getElementsByClassName("clouds");
@@ -48,8 +52,8 @@ const Weather = () => {
         let description = document.getElementsByClassName("description");
         Temperature[0].innerHTML = Math.floor(data.main.temp - 273.15);
         Feelslike[0].innerHTML = Math.floor(data.main.feels_like - 273.15);
-        wind[0].innerHTML = data.wind.speed + " km/h";
-        Humidity[0].innerHTML = data.main.humidity + " %";
+        wind[0].innerHTML = data.wind.speed + "km/h";
+        Humidity[0].innerHTML = data.main.humidity + "%";
         City_Name[0].innerHTML = data.name + " " + data.sys.country;
         description[0].innerHTML = data.weather[0].description;
 
@@ -57,18 +61,20 @@ const Weather = () => {
           setBgImage(clearSkyimg);
         } else if (data.weather[0].icon === "02d") {
           setBgImage(fewClouds);
-        } else if (data.weather[0].description === "03d") {
+        } else if (data.weather[0].icon === "03d") {
           setBgImage(scatteredClouds);
-        } else if (data.weather[0].description === "04d") {
+        } else if (data.weather[0].icon === "04d") {
           setBgImage(brokenCLouds);
         } else if (
           data.weather[0].icon === "09d" ||
-          data.weather[0].icon === "10d"
+          data.weather[0].icon === "10d" ||
+          data.weather[0].icon === "09n" ||
+          data.weather[0].icon === "10n"
         ) {
           setBgImage(rainImg);
-        } else if (data.weather[0].description === "11d") {
+        } else if (data.weather[0].icon === "11d") {
           setBgImage(ThunderStorm);
-        } else if (data.weather[0].description === "13d") {
+        } else if (data.weather[0].icon === "13d") {
           setBgImage(snowBg);
         } else if (data.weather[0].description === "overcast clouds") {
           setBgImage(overcast);
@@ -83,6 +89,7 @@ const Weather = () => {
         setError("Something went wrong..Try again");
       }
     }
+    console.log(bgImage);
   };
 
   return (
@@ -94,64 +101,66 @@ const Weather = () => {
           alt="background image"
           style={{ width: "100%", height: "100vh" }}
         />
-
-        <p className="weather header text-4xl tracking-widest text-white ml-10 italic ">
-          Weather
-        </p>
-        {/* GET DATE FUNCTION DISPLAY */}
-        {/* <p className="weather date text-1xl font-semibold tracking-widest text-white italic ml-60">
-          {currentDate}
-        </p> */}
-        <span className="cityInput weather top-16 ml-10 ">
-          Search for a new city
-        </span>
-        <input
-          placeholder="Enter City Name"
-          className="absolute top-24 ml-10 text-black px-10 py-2 rounded-md outline-none py inputField"
-          value={city}
-          onChange={(e) => {
-            setCity(e.target.value);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSearch();
-            }
-          }}
-        />
-        <div className="SearchIconDiv absolute w-6 left-72">
-          <img
-            src="https://ouch-cdn2.icons8.com/AJnCJKWQF4zk3jLDQCBaO7hkeyEHYIIHb5YXNWOLtHo/rs:fit:368:378/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTg2/L2Y0MTk0NDFlLTMz/NTYtNGZmMC04ZmIx/LWE4YzNjY2VkODU0/OC5zdmc.png"
-            alt="search icon"
-            onClick={handleSearch}
+        <label className="input input-bordered flex items-center gap-2 absolute top-10 ml-5">
+          <input
+            type="text"
+            className="grow px-10 py-2 rounded-md outline-none"
+            placeholder="Search"
+            value={city}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSearch();
+              }
+            }}
           />
-        </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+            onClick={handleSearch}
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
 
-        <div className="absolute mt-11 text-white top-32 ml-10 textDiv ">
-          <h1 className=" text-8xl tempClass  ">
-            <span className="temprature">NA</span>
-            <span>&#176;C</span>
-          </h1>
-          <p className="text-2xl mt-4">
-            feels like <span className="feelslike">NA</span>
-            <span>&#176;C</span>
-          </p>
+        <div className="container absolute p-3">
+          <div className="content p-4">
+            <h1 className=" text-8xl tempClass mb-3  ">
+              <span className="temprature">NA</span>
+              <span>&#176;C</span>
+            </h1>
+            <span className="text-xl currentcity">CITY</span>
+            <p className="mt-4">
+              feels like <span className="feelslike">NA</span>
+              <span>&#176;C</span>
+            </p>
 
-          <p className=" mt-8">
-            Wind Speed: <span className="clouds">NA</span>
-          </p>
-          <p>
-            Humidity: <span className="humidity">NA</span>
-          </p>
-          <p className="description"></p>
+            <p className=" mt-8">
+              Wind Speed: <span className="clouds">NA</span>
+            </p>
+            <p>
+              Humidity: <span className="humidity">NA</span>
+            </p>
+            <p className="description"></p>
+            <p className="weather date text-1xl tracking-widest text-blue-400">
+              {currentDate}
+            </p>
+          </div>
+
           {error && (
             <div style={{ color: "red" }} className="text-1xl errMsg">
               {error}
             </div>
           )}
         </div>
-        <span className="absolute top-96 text-4xl text-white CurrentCity tracking-widest">
-          <span className="currentcity">CITY</span>
-        </span>
       </div>
     </>
   );
